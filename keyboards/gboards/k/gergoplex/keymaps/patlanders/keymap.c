90dd1324a7277197321dea0a49ad1aa898b2c2c1
Pat Landers <landers.colin@gmail.com>
2021-10-01 12:18:11 -0700

    Add num row to plover layer.

--- keyboards/gboards/k/gergoplex/keymaps/patlanders/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/patlanders/keymap.c
@@ -124,20 +124,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 4: Plover layer
  * ,-----------------------------.      ,-------------------------------.
- * |  #  |  #  |  #  |  #  |  #  |      |  *  |  #  |  #  |  #  |   #   |
+ * |  1  |  2  |  3  |  4  |  5  |      |  *  |  6  |  7  |  8  |   9   |
  * |-----+- ---+-----+-----+-----|      |-------------------------------|
  * |  Q  |  W  |  E  |  R  |  T  |      |  U  |  I  |  O  |  P  |   [   |
  * |-----+-----+-----+-----+-----+      |-------------------------------|
  * |  A  |  S  |  D  |  F  |  G  |      |  J |   K  |  L  |  ;  |   '   |
  * `-----+-----+-----+-----+-----'      `-------------------------------'
  *          .-----------------.           .---------------.
- *          | C |  V  | BASE  |           | BASE |  N  | M|
+ *          | BASE |  C  | V  |           | N |  M  | BASE|
  *          '-----------------'           '---------------'
  */
 [PLVR] = LAYOUT_gergoplex(
-    KC_NUHS,  KC_NUHS,  KC_NUHS,  KC_NUHS,  KC_NUHS,		 		KC_T,  	KC_NUHS, 	 KC_NUHS, 		KC_NUHS, 	 KC_NUHS,
+    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,		 		KC_T,  	KC_7, 	 KC_8, 		KC_8, 	 KC_9,
     KC_Q, KC_W, KC_E, KC_R, KC_T,   		KC_U, KC_I,   KC_O, KC_P, KC_LBRC,
     KC_A, KC_S, KC_D, KC_F, KC_G,  		KC_J, KC_K, KC_L, KC_SCLN,KC_QUOTE,
-    KC_C,KC_V,TO(BASE),  	                        TO(BASE),KC_N,KC_M
+    TO(BASE), KC_C,KC_V,  	                       KC_N, KC_M, TO(BASE)
 			  )
 };
